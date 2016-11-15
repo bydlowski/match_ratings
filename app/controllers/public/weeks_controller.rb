f673a99115b691555541d0c31fece8a2efb49c85
Pedro Bydlowki <pedro@bydlowski.com>
2016-11-15 15:38:58 -0200

    Back button

--- app/controllers/public/weeks_controller.rb
+++ app/controllers/public/weeks_controller.rb
@@ -17,5 +17,10 @@ class Public::WeeksController < ApplicationController
     @array = ArrayGame.first
     @games_array = @array.game_url
     @stats = GameData.where(game_week_number: params[:number])
+    save_my_previous_url
+  end
+  def save_my_previous_url
+    #session[:my_previous_url] = URI(request.referer || '').path
+    session[:my_previous_url] = URI(request.url || '').path
   end
 end
