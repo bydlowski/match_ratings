b175890c9a7bc0e350c32b4a59e748cf65f2dff4
Pedro Bydlowki <pedro@bydlowski.com>
2016-11-15 15:09:58 -0200

    Full stats

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,5 +1,5 @@
 module GamesHelper
-  def score(date,time,game,counter,algo,user_team)
+  def score(date,time,game,counter,algo,user_team,week)
     page = ''
     year = date.split('-').first
     month = date.split('-').second
@@ -9,7 +9,7 @@ module GamesHelper
     yellow_score = "col xs-col-4 xs-text-2 fill-yellow-lighter xs-border-left-lighter"
     green_score = "col xs-col-4 xs-text-2 new-green xs-border-left-lighter"
     game_date = DateTime.new(year.to_i, month.to_i, day.to_i)
-    rating = game_rating(game,algo,user_team)
+    rating = game_rating(game,algo,user_team,week)
     #rating = (((game.home_team_score+game.away_team_score).abs)*0.3 + score_diff(game.home_team_score,game.away_team_score,game.quarter_count) + down_diff((game.stats_home_team_downs-game.stats_away_team_downs).abs) + game.stats_interceptions*2 + game.stats_fumbles*1.5).round(0)
 
     if (game_date.wday == 4 || game_date.wday == 1 || time == '8:30PM' || time == '9:30AM') && session[:hide_score_value] == 'HideR'
@@ -89,7 +89,7 @@ module GamesHelper
     end
   end
 
-  def game_rating(game,algo,user_team)
+  def game_rating(game,algo,user_team,week)
     total_score = ((game.home_team_score+game.away_team_score).abs) * 0.3
     score_diferential = score_diff(game.home_team_score,game.away_team_score,game.quarter_count)
     down_difference = down_diff((game.stats_home_team_downs-game.stats_away_team_downs).abs)
@@ -98,48 +98,75 @@ module GamesHelper
     chosen_team = (game.away_team_abrev == user_team || game.home_team_abrev == user_team) ? 3 : 0
 
     if algo == 'FullAlgo'
-      rivals(game.home_team_abrev, game.away_team_abrev)
+      #rivals(game.home_team_abrev, game.away_team_abrev)
+      #divisional(game.home_team_abrev, game.away_team_abrev)
       #chosen_team
+      #win_loss(week,game.home_team_abrev,game.away_team_abrev)
+      (rivals(game.home_team_abrev, game.away_team_abrev) + divisional(game.home_team_abrev, game.away_team_abrev) + chosen_team + win_loss(week,game.home_team_abrev,game.away_team_abrev) + total_score + score_diferential + down_difference + intercaptions + fumbles).round(0)
     else
       (total_score + score_diferential + down_difference + intercaptions + fumbles).round(0)
     end
   end
 
   def rivals(home, away)
-    rivalry_array = [['CHI','GB',3],['DEN','OAK',3],['CLE','CIN',2],['PIT','BAL',4],['WAS','DAL',3],['SF','SEA',1],['PHI','NYG',1],['NE','NYJ',3],['PIT','CLE',1],['MIN','GB',1],['ATL','NO',1],['BUF','MIA',1],['DEN','MIA',1],['DAL','PHI',1]]
+    rivalry_array = [['CHI','GB',5],['DEN','OAK',3],['CLE','CIN',3],['PIT','BAL',5],['WAS','DAL',4],['SF','SEA',2],['PHI','NYG',2],['NE','NYJ',3],['PIT','CLE',2],['MIN','GB',1],['ATL','NO',3],['BUF','MIA',1],['DEN','MIA',1],['DAL','PHI',1],['BAL','NE',2],['NE','IND',1],['DAL','NYG',2],['OAK','KC',1],['TEN','HOU',1],['NYJ','NYG',1],['DET','GB',1]]
     rivalry_array.each do |game|
       score = 0
       result = 0
       game.each do |team|
         if team == home
           score += 1
-          #result + team.to_s
         elsif team == away
           score += 1
-          #result + team.to_s
         elsif team.is_a? Integer
           result += team
         end
       end
       if score == 2
-        #return home
         return result.to_i
       else
         return 0
       end
-      # game.each do |team|
-      #   if team == home || team == away
-      #     score += 1
-      #   end
-      #   if score > 1
-      #     return team
-      #   end
-      # end
-      # if game[1] == home || game[2] == home
-      #   if game[1] == away || game[2] == away
-      #     return game[3].to_a
-      #   end
-      # end
+    end
+  end
+
+  def divisional(home,away)
+    divisions_array = [['NE','CIN','HOU','DEN'],['NYJ','PIT','IND','KC'],['BUF','BAL','JAX','OAK'],['MIA','CLE','TEN','SD'],['WAS','MIN','CAR','ARI'],['PHI','GB','ATL','SEA'],['NYG','DET','NO','LA'],['DAL','CHI','TB','SF']]
+    divisions_array.each do |game|
+      score = 0
+      game.each do |team|
+        if team == home
+          score += 1
+        elsif team == away
+          score += 1
+        end
+      end
+      if score == 2
+        return 3
+      else
+        return 0
+      end
+    end
+  end
+
+  def win_loss(week,home,away)
+    home_record = team_stats_week(home,week)
+    away_record = team_stats_week(away,week)
+    home_games = (home_record.split('-').first).to_i + (home_record.split('-').second).to_i
+    home_coef = (home_record.split('-').first).to_i - (home_record.split('-').second).to_i
+    away_games = (away_record.split('-').first).to_i + (away_record.split('-').second).to_i
+    away_coef = (away_record.split('-').first).to_i - (away_record.split('-').second).to_i
+    return ((win_loss_score(home_games,home_coef) + win_loss_score(away_games,away_coef)) * 5).round(1)
+  end
+
+  def win_loss_score(games,coef)
+    if games < 3
+      0
+    elsif (coef.to_f / games.to_f) > 0.3
+      0.3
+      #(coef.to_f / games.to_f)
+    else
+      (coef.to_f / games.to_f)
     end
   end
 
