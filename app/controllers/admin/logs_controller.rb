db29f15d6e2ae65fe823e87beb5dd9792af79472
Pedro Bydlowki <pedro@bydlowski.com>
2017-09-12 17:51:27 -0300

    Log order

--- app/controllers/admin/logs_controller.rb
+++ app/controllers/admin/logs_controller.rb
@@ -2,6 +2,6 @@ class Admin::LogsController < ApplicationController
   layout 'admin'
   def index
     ten_days = Date.today - 10
-    @logs = AdminLogs.where(:log_date.gte => ten_days)
+    @logs = AdminLogs.where(:log_date.gte => ten_days).desc(:log_date)
   end
 end
